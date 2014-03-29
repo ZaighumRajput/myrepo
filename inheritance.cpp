fbfb30c9d928f3d005cb477e80c3c6289e1e1018
Zaighum <zaighum47@gmail.com>
2014-03-30 00:06:31 +0100

    first commit

---
+++ inheritance.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+
+class mother {
+    
+public:
+  mother(std::string s) { s = lastName;}
+    void getInfo() {
+      std::cout << "last Name is :";  
+      std::cout << lastName;
+  
+    }
+    
+    
+private:
+    std::string lastName;
+    
+};
+
+class daughter : public mother{
+    
+
+};
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    mother chachi("munir");
+    chachi.getInfo();
+}
